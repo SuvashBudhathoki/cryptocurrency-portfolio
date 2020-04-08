0a7f7c850feb07ad7a4cbf332b75e525ce52169b
Suvash Budhathoki <SuvashBudhathoki@users.noreply.github.com>
2020-04-08 14:12:38 +1000

    private route set to give access to authenticated users only

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,24 +3,26 @@ import { Router, Route, Switch } from "react-router-dom";
 import Dashboard from "../components/Dashboard";
 import AddTransaction from "../components/AddTransaction";
 import EditTransaction from "../components/EditTransaction";
-import Header from "../components/Header";
 import RenderCoinMarketCapApi from "../components/RenderCoinMarketCapApi";
 import NotFoundPage from "../components/NotFoundPage";
 import LoginPage from "../components/LoginPage";
 import createHistory from "history/createBrowserHistory";
+import PrivateRoute from "./PrivateRoute";
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/create" component={AddTransaction} exact={true} />
-        <Route path="/edit/:id" component={EditTransaction} />
-        <Route path="/coin-market-api" component={RenderCoinMarketCapApi} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/create" component={AddTransaction} exact={true} />
+        <PrivateRoute path="/edit/:id" component={EditTransaction} />
+        <PrivateRoute
+          path="/coin-market-api"
+          component={RenderCoinMarketCapApi}
+        />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
